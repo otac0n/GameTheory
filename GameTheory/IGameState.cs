dfa1d8de9968ae4a09d70cd06c68828d0bdb331f
John Gietzen <otac0n@gietzen.us>
2014-12-15 18:36:18 -0800

    Created the GameTheory project.

---
+++ GameTheory/IGameState.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IGameState.cs" company="(none)">
+//   Copyright © 2014 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GameTheory
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents the contract of a game state.
+    /// </summary>
+    /// <typeparam name="TMove">The type of object that represents a move.</typeparam>
+    public interface IGameState<TMove> where TMove : IMove
+    {
+        /// <summary>
+        /// Returns a finite, enumerable collection of moves.
+        /// </summary>
+        /// <returns>An enumerable list of moves available.</returns>
+        IEnumerable<TMove> GetAvailableMoves();
+
+        /// <summary>
+        /// Applies the specified move to the given game state and returns the result.
+        /// </summary>
+        /// <param name="move">The move to apply.</param>
+        /// <returns>The modified game state.</returns>
+        IGameState<TMove> Move(TMove move);
+    }
+}
---
+++ GameTheory/IMove.cs
@@ -0,0 +1,17 @@
+// -----------------------------------------------------------------------
+// <copyright file="IMove.cs" company="(none)">
+//   Copyright © 2014 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GameTheory
+{
+    /// <summary>
+    /// Represents the contract of a move.
+    /// </summary>
+    public interface IMove
+    {
+    }
+}
