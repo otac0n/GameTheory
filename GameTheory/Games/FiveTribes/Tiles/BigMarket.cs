21ad19cb5c4afbaa42f9db0f43da4f0770003f51
John Gietzen <john@gietzen.us>
2016-09-04 12:05:55 -0700

    Add missing files.

---
+++ GameTheory/Games/FiveTribes/Tiles/BigMarket.cs
@@ -0,0 +1,70 @@
+﻿// Copyright © 2016 John Gietzen.  All Rights Reserved.
+// This source is subject to the MIT license.
+// Please see license.md for more information.
+
+namespace GameTheory.Games.FiveTribes.Tiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using GameTheory.Games.FiveTribes.Moves;
+
+    /// <summary>
+    /// Represents a Big Market tile.
+    /// </summary>
+    public class BigMarket : Tile
+    {
+        /// <summary>
+        /// The number of <see cref="Resource">Resources</see> the player can choose from, starting from the beginning.
+        /// </summary>
+        public const int FirstN = 6;
+
+        /// <summary>
+        /// The cost of using the <see cref="BigMarket"/> to purchase two <see cref="Resource">Resources</see>.
+        /// </summary>
+        public const int Gold = 6;
+
+        /// <summary>
+        /// The singleton instance of <see cref="BigMarket"/>.
+        /// </summary>
+        public static readonly BigMarket Instance = new BigMarket();
+
+        private BigMarket()
+            : base(4, TileColor.Red)
+        {
+        }
+
+        /// <inheritdoc />
+        public override IEnumerable<Move> GetTileActionMoves(GameState state)
+        {
+            if (state.VisibleResources.Count > 0)
+            {
+                var moves = Cost.Gold(state, Gold, s => s, s1 => from i in Enumerable.Range(0, Math.Min(FirstN, s1.VisibleResources.Count))
+                                                                 select new TakeResourceMove(s1, i, s2 =>
+                                                                 {
+                                                                     if (s2.VisibleResources.Count >= 1)
+                                                                     {
+                                                                         return s2.WithMoves(s3 => Enumerable.Concat(
+                                                                             from j in Enumerable.Range(0, Math.Min(FirstN - 1, s3.VisibleResources.Count))
+                                                                             select new TakeResourceMove(s3, j, s4 => s4.With(phase: Phase.CleanUp)),
+                                                                             new Move[] { new ChangePhaseMove(s3, "Skip second resource", Phase.CleanUp) }));
+                                                                     }
+                                                                     else
+                                                                     {
+                                                                         return s2.With(phase: Phase.CleanUp);
+                                                                     }
+                                                                 }));
+
+                foreach (var m in moves)
+                {
+                    yield return m;
+                }
+            }
+
+            foreach (var m in base.GetTileActionMoves(state))
+            {
+                yield return m;
+            }
+        }
+    }
+}
---
+++ GameTheory/Games/FiveTribes/Tiles/Tile.cs
@@ -0,0 +1,55 @@
+﻿// Copyright © 2016 John Gietzen.  All Rights Reserved.
+// This source is subject to the MIT license.
+// Please see license.md for more information.
+
+namespace GameTheory.Games.FiveTribes.Tiles
+{
+    using System.Collections.Generic;
+    using GameTheory.Games.FiveTribes.Moves;
+
+    /// <summary>
+    /// Represents a tile in game of Five Tribes.
+    /// </summary>
+    public abstract class Tile
+    {
+        private readonly TileColor color;
+        private readonly int value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Tile"/> class.
+        /// </summary>
+        /// <param name="value">The value of the <see cref="Tile"/>, in Victory Points (VP).</param>
+        /// <param name="color">The color of the <see cref="Tile"/>.</param>
+        protected Tile(int value, TileColor color)
+        {
+            this.value = value;
+            this.color = color;
+        }
+
+        /// <summary>
+        /// Gets the color of the <see cref="Tile"/>.
+        /// </summary>
+        public TileColor Color
+        {
+            get { return this.color; }
+        }
+
+        /// <summary>
+        /// Gets the value of the <see cref="Tile"/>, in Victory Points (VP).
+        /// </summary>
+        public int Value
+        {
+            get { return this.value; }
+        }
+
+        /// <summary>
+        /// Generates moves for the specified <see cref="GameState"/>.
+        /// </summary>
+        /// <param name="state">The <see cref="GameState"/> for which <see cref="Move">Moves</see> are being generated.</param>
+        /// <returns>The <see cref="Move">Moves</see> provided by the <see cref="Tile"/>.</returns>
+        public virtual IEnumerable<Move> GetTileActionMoves(GameState state)
+        {
+            yield return new ChangePhaseMove(state, "Skip Tile Action", Phase.CleanUp);
+        }
+    }
+}
