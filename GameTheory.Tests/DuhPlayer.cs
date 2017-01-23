3097321cfae542a232f78d94eca7a3aa39c38e70
John Gietzen <john@gietzen.us>
2017-01-23 00:22:24 -0800

    Added a min-max-like player.  Currently supports single or multiplayer complete, perfect information games with non-simultaneous moves.

---
+++ GameTheory.Tests/DuhPlayer.cs
@@ -0,0 +1,16 @@
+﻿// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GameTheory.Tests
+{
+    using GameTheory.Players;
+    using GameTheory.Strategies;
+
+    internal class DuhPlayer<TMove> : StrategyPlayer<TMove>
+        where TMove : IMove
+    {
+        public DuhPlayer(PlayerToken playerToken)
+            : base(new ImmediateWinStrategy<TMove>(), new RandomPlayer<TMove>(playerToken))
+        {
+        }
+    }
+}
