a460c1166aaaeb3709775c32b3aae8f58426fd00
John Gietzen <john@gietzen.us>
2017-05-21 13:14:32 -0700

    Added the matching pennies game.

---
+++ GameTheory/Games/MatchingPennies/GameState.cs
@@ -0,0 +1,118 @@
+﻿// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GameTheory.Games.MatchingPennies
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+
+    /// <summary>
+    /// Implements the game of matching pennies.
+    /// </summary>
+    public class GameState : IGameState<Move>
+    {
+        private readonly ImmutableArray<PlayerToken> players;
+        private readonly ImmutableArray<bool?> choices;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameState"/> class in the starting position.
+        /// </summary>
+        public GameState()
+            : this(ImmutableArray.Create(new PlayerToken(), new PlayerToken()), ImmutableArray.Create<bool?>(null, null))
+        {
+        }
+
+        private GameState(ImmutableArray<PlayerToken> players, ImmutableArray<bool?> choices)
+        {
+            this.players = players;
+            this.choices = choices;
+        }
+
+        /// <inheritdoc />
+        public IReadOnlyList<PlayerToken> Players => this.players;
+
+        /// <inheritdoc />
+        public IReadOnlyCollection<Move> GetAvailableMoves(PlayerToken playerToken)
+        {
+            var index = this.players.IndexOf(playerToken);
+            if (index == -1)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (this.choices[index] != null)
+            {
+                return ImmutableArray<Move>.Empty;
+            }
+
+            return ImmutableArray.Create(new Move(playerToken, true), new Move(playerToken, false));
+        }
+
+        /// <inheritdoc />
+        public IReadOnlyCollection<PlayerToken> GetWinners()
+        {
+            if (this.choices.Any(c => c == null))
+            {
+                return ImmutableArray<PlayerToken>.Empty;
+            }
+
+            return ImmutableArray.Create(this.players[this.choices.Distinct().Count() - 1]);
+        }
+
+        /// <inheritdoc />
+        IGameState<Move> IGameState<Move>.MakeMove(Move move) => this.MakeMove(move);
+
+        /// <summary>
+        /// Applies the move to the current game state.
+        /// </summary>
+        /// <param name="move">The <see cref="Move"/> to apply.</param>
+        /// <returns>The updated <see cref="GameState"/>.</returns>
+        public GameState MakeMove(Move move)
+        {
+            if (move == null)
+            {
+                throw new ArgumentNullException(nameof(move));
+            }
+
+            var index = this.players.IndexOf(move.PlayerToken);
+
+            return new GameState(
+                players: this.players,
+                choices: this.choices.SetItem(index, move.Heads));
+        }
+
+        /// <inheritdoc />
+        public IGameState<Move> GetView(PlayerToken playerToken)
+        {
+            var index = this.players.IndexOf(playerToken);
+            if (index == -1)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return new GameState(
+                players: this.players,
+                choices: this.choices.SetItem(1 - index, null));
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            string Penny(bool? choice)
+            {
+                switch (choice)
+                {
+                    case true:
+                        return "H";
+                    case false:
+                        return "T";
+                    default:
+                        return "?";
+                }
+            }
+
+            return $"[{Penny(this.choices[0])}{Penny(this.choices[1])}]";
+        }
+    }
+}
---
+++ GameTheory/Games/MatchingPennies/Move.cs
@@ -0,0 +1,32 @@
+﻿// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GameTheory.Games.MatchingPennies
+{
+    /// <summary>
+    /// Represents a move in Matching Pennies.
+    /// </summary>
+    public class Move : IMove
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Move"/> class.
+        /// </summary>
+        /// <param name="playerToken">The player who may make this move.</param>
+        /// <param name="heads">A value indicating whether or not the player has chosen heads.</param>
+        public Move(PlayerToken playerToken, bool heads)
+        {
+            this.PlayerToken = playerToken;
+            this.Heads = heads;
+        }
+
+        /// <inheritdoc />
+        public PlayerToken PlayerToken { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the player has chosen heads.
+        /// </summary>
+        public bool Heads { get; }
+
+        /// <inheritdoc />
+        public override string ToString() => this.Heads ? "H" : "T";
+    }
+}
