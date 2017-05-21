df003740ed8860fd20e8d207cfe05d2d56cad47e
John Gietzen <john@gietzen.us>
2017-05-20 23:54:36 -0700

    Fix game end condition for Mancala.

--- GameTheory.Tests/Games/MancalaTests.cs
+++ GameTheory.Tests/Games/MancalaTests.cs
@@ -24,5 +24,18 @@ namespace GameTheory.Tests.Games
 
             Assert.That(winners, Is.EqualTo(endState.Players.Where(p => endState.GetScore(p) == highestScore)));
         }
+
+        [TestCase("(5),(7),(4),(7),(3),(12),(0),(7),(1),(7),(2),(7),(3),(7),(4),(7),(5)")]
+        [TestCase("(1),(9),(12),(0),(8),(3),(12),(9),(2),(7),(12),(10),(0),(11),(1),(5),(2),(12),(0),(9),(5),(2),(10),(4),(12),(11),(12),(9),(0),(7),(2),(8),(12),(10),(12),(11),(12),(9),(1),(10),(2),(11),(5),(3),(7),(5),(4),(8),(5)")]
+        public void GetAvailableMoves_WhenOnePlayersSideIsEmpty_YieldsNone(string moves)
+        {
+            var state = new GameState();
+            foreach (var move in moves.Split(','))
+            {
+                state = state.MakeMove(state.GetAvailableMoves().Where(m => m.ToString().Contains(move)).Single());
+            }
+
+            Assert.That(state.GetAvailableMoves(), Is.Empty);
+        }
     }
 }
--- GameTheory/Games/Mancala/Move.cs
+++ GameTheory/Games/Mancala/Move.cs
@@ -47,6 +47,7 @@ namespace GameTheory.Games.Mancala
             var captureSpots = new HashSet<int>(state.GetPlayerIndexes(this.PlayerToken).Take(GameState.BinsOnASide));
             var otherPlayer = state.Players.Except(this.PlayerToken).Single();
             var otherMancala = state.GetPlayerIndexes(otherPlayer).Last();
+            var othersBins = new HashSet<int>(state.GetPlayerIndexes(otherPlayer).Take(GameState.BinsOnASide));
 
             var bin = this.Bin;
             var board = state.Board;
@@ -67,10 +68,10 @@ namespace GameTheory.Games.Mancala
                 count -= 1;
             }
 
+            PlayerToken activePlayer;
             if (bin == mancala)
             {
-                return state.With(
-                    board: board);
+                activePlayer = this.PlayerToken;
             }
             else
             {
@@ -86,10 +87,31 @@ namespace GameTheory.Games.Mancala
                     }
                 }
 
-                return state.With(
-                    activePlayer: otherPlayer,
-                    board: board);
+                activePlayer = otherPlayer;
             }
+
+            if (captureSpots.All(i => board[i] == 0))
+            {
+                foreach (var i in othersBins)
+                {
+                    board = board
+                        .SetItem(otherMancala, board[otherMancala] + board[i])
+                        .SetItem(i, 0);
+                }
+            }
+            else if (othersBins.All(i => board[i] == 0))
+            {
+                foreach (var i in captureSpots)
+                {
+                    board = board
+                        .SetItem(mancala, board[mancala] + board[i])
+                        .SetItem(i, 0);
+                }
+            }
+
+            return state.With(
+                board: board,
+                activePlayer: activePlayer);
         }
     }
 }
