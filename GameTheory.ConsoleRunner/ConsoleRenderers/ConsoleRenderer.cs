69c88bd5fbbe978a47d63b73f63292d9f47bdbb0
John Gietzen <john@gietzen.us>
2017-05-28 10:49:10 -0700

    Made Console Renderer more generic with reflection.

--- GameTheory.ConsoleRunner/ConsoleRenderers/ConsoleRenderer.cs
+++ GameTheory.ConsoleRunner/ConsoleRenderers/ConsoleRenderer.cs
@@ -2,14 +2,26 @@
 
 namespace GameTheory.ConsoleRunner.ConsoleRenderers
 {
+    using System;
+    using System.Collections.Immutable;
+    using System.Linq;
+
     internal static class ConsoleRenderer
     {
+        private static readonly IImmutableDictionary<Type, Type> DefaultMap = (from t in typeof(IConsoleRenderer<>).Assembly.ExportedTypes
+                                                                               where !t.IsAbstract
+                                                                               where t.GetConstructors().Any(c => c.IsPublic && c.GetParameters().Length == 0)
+                                                                               from i in t.GetInterfaces()
+                                                                               where i.IsConstructedGenericType && i.GetGenericTypeDefinition() == typeof(IConsoleRenderer<>)
+                                                                               let m = i.GetGenericArguments()[0]
+                                                                               select new { t, m }).ToImmutableDictionary(x => x.m, x => x.t);
+
         internal static IConsoleRenderer<TMove> Default<TMove>()
             where TMove : IMove
         {
-            if (typeof(TMove) == typeof(Games.Splendor.Move))
+            if (DefaultMap.TryGetValue(typeof(TMove), out Type rendererType))
             {
-                return (IConsoleRenderer<TMove>)new SplendorConsoleRenderer();
+                return (IConsoleRenderer<TMove>)Activator.CreateInstance(rendererType);
             }
 
             return new ToStringConsoleRenderer<TMove>();
