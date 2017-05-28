b1e6848aca0eaeabd18283391dfbd64e441dd8c8
John Gietzen <john@gietzen.us>
2017-05-27 20:02:02 -0700

    Fix warning.

--- GameTheory.ConsoleRunner/ConsoleRenderers/IConsoleRenderer.cs
+++ GameTheory.ConsoleRunner/ConsoleRenderers/IConsoleRenderer.cs
@@ -1,4 +1,6 @@
-﻿namespace GameTheory.ConsoleRunner.ConsoleRenderers
+﻿// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GameTheory.ConsoleRunner.ConsoleRenderers
 {
     internal interface IConsoleRenderer<TMove>
         where TMove : IMove
