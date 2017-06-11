14738da1ba766315afd577d73b3ed0e24e56f050
John Gietzen <john@gietzen.us>
2017-06-11 10:56:13 -0700

    Created basic maximizing players for Five Tribes and Splendor.

---
+++ GameTheory/Players/MaximizingPlayers/SplendorMaximizingPlayer.cs
@@ -0,0 +1,45 @@
+﻿// Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GameTheory.Players.MaximizingPlayers
+{
+    using System.Linq;
+    using Games.Splendor;
+
+    /// <summary>
+    /// A maximizing player for the game of <see cref="GameState">Splendor</see>.
+    /// </summary>
+    public class SplendorMaximizingPlayer : MaximizingPlayer<Move, double>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SplendorMaximizingPlayer"/> class.
+        /// </summary>
+        /// <param name="playerToken">The token that represents the player.</param>
+        /// <param name="minPly">The minimum number of ply to think ahead.</param>
+        public SplendorMaximizingPlayer(PlayerToken playerToken, int minPly)
+            : base(playerToken, new ScoringMetric(), minPly)
+        {
+        }
+
+        private class ScoringMetric : IScoringMetric
+        {
+            /// <inheritdoc/>
+            public double CombineScores(IWeighted<double>[] scores) =>
+                scores.Sum(s => s.Value * s.Weight) / scores.Sum(s => s.Weight);
+
+            /// <inheritdoc/>
+            public int Compare(double x, double y) =>
+                x.CompareTo(y);
+
+            /// <inheritdoc/>
+            public double Difference(double playerScore, double opponentScore) =>
+                playerScore - opponentScore;
+
+            /// <inheritdoc/>
+            public double Score(IGameState<Move> state, PlayerToken playerToken)
+            {
+                var gameState = (GameState)state;
+                return gameState.GetScore(playerToken);
+            }
+        }
+    }
+}
