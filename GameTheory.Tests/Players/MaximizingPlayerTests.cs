784e23ed60ddedc1164d6fa1a95fa6f1b2ae423b
John Gietzen <john@gietzen.us>
2017-08-12 23:54:51 -0700

    Added tests for ideal behavior of Maximizing player around certain win / certain loss.

--- GameTheory.Tests/Players/MaximizingPlayerTests.cs
+++ GameTheory.Tests/Players/MaximizingPlayerTests.cs
@@ -4,6 +4,7 @@ namespace GameTheory.Tests.Players
 {
     using System;
     using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     using GameTheory.Games.TicTacToe;
     using GameTheory.Players.MaximizingPlayers;
@@ -58,6 +59,58 @@ namespace GameTheory.Tests.Players
             }
 
             Assert.That(wins[0], Is.Zero);
+        }
+
+        [Test]
+        public async Task WhenLossCanBeAverted_Always_AvoidsImmediateLoss(
+            [Values("(0, 0);(1, 1);(2, 2);(2, 0);(0, 2)", "(0, 0);(1, 1);(2, 2);(1, 2);(1, 0)")] string moves,
+            [Values(2, 3, 4, 5, 6)] int ply)
+        {
+            await InitializeStateAndAssertNextMove(moves, ply);
+        }
+
+        [Test]
+        public async Task WhenLossCannotBeAverted_Always_AvoidsImmediateLoss(
+            [Values("(2, 1);(2, 2);(0, 1);(1, 1);(0, 0)", "(2, 1);(2, 2);(2, 0);(1, 2);(0, 2)")] string moves,
+            [Values(2, 3, 4, 5, 6)] int ply)
+        {
+            await InitializeStateAndAssertNextMove(moves, ply);
+        }
+
+        [Test]
+        public async Task WhenMultipleWinsAreAvailable_Always_ChoosesTheSoonestWin(
+            [Values("(0, 0);(0, 1);(2, 0);(2, 1);(1, 0)", "(0, 0);(0, 2);(2, 0);(0, 1);(1, 0)")] string moves,
+            [Values(2, 3, 4, 5, 6)] int ply)
+        {
+            await InitializeStateAndAssertNextMove(moves, ply);
+        }
+
+        private static async Task InitializeStateAndAssertNextMove(string moves, int ply)
+        {
+            var state = new GameState();
+            var playerX = state.Players[0];
+            var playerO = state.Players[1];
+            var i = 0;
+            PlayerToken playerToken() => i++ % 2 == 0 ? playerX : playerO;
+            var movesArray = moves.Split(';');
+            var nextMove = movesArray.Last();
+            Array.Resize(ref movesArray, movesArray.Length - 1);
+            state = (GameState)movesArray.Aggregate((IGameState<Move>)state, (newState, move) => newState.PlayMove(playerToken(), m => m.ToString() == move));
+
+            var token = playerToken();
+            var success = 0;
+            for (var samples = Samples; samples > 0; samples--)
+            {
+                var player = new TicTacToeMaximizingPlayer(token, minPly: ply);
+                var cancel = new CancellationTokenSource();
+                var chosenMove = await player.ChooseMove(state, cancel.Token);
+                if (chosenMove.Value.ToString() == nextMove)
+                {
+                    success++;
+                }
+            }
+
+            Assert.That(success, Is.EqualTo(Samples));
         }
     }
 }
