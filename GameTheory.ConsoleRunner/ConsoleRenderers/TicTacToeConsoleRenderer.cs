f5ec4a87d4b4f7bab4ab7dc187468fde902ae78a
John Gietzen <john@gietzen.us>
2017-08-27 21:05:15 -0700

    Render Tic-tac-toe player names as "X" and "O".

---
+++ GameTheory.ConsoleRunner/ConsoleRenderers/TicTacToeConsoleRenderer.cs
@@ -0,0 +1,35 @@
+﻿// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GameTheory.ConsoleRunner.ConsoleRenderers
+{
+    using System;
+    using Games.TicTacToe;
+
+    /// <summary>
+    /// Implements a console renderer for the game of <see cref="GameState">TicTacToe</see>.
+    /// </summary>
+    public class TicTacToeConsoleRenderer : BaseConsoleRenderer<Move>
+    {
+        /// <inheritdoc/>
+        public override void Show(IGameState<Move> state, PlayerToken playerToken = null)
+        {
+            Console.WriteLine(state);
+        }
+
+        /// <inheritdoc/>
+        protected override void RenderToken(IGameState<Move> state, object token)
+        {
+            if (token is PlayerToken playerToken)
+            {
+                ConsoleInteraction.WithColor(ConsoleInteraction.GetPlayerColor(state, playerToken), () =>
+                {
+                    Console.Write(playerToken == state.Players[0] ? "X" : "O");
+                });
+            }
+            else
+            {
+                base.RenderToken(state, token);
+            }
+        }
+    }
+}
