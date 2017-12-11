d42ef146ae3898de700f4b7cc8251af957cf5fe1
John Gietzen <john@gietzen.us>
2017-12-10 20:37:53 -0800

    Add DevelopmentLevel to Splendor.

--- GameTheory/Games/Splendor/DevelopmentCard.cs
+++ GameTheory/Games/Splendor/DevelopmentCard.cs
@@ -6,6 +6,27 @@ namespace GameTheory.Games.Splendor
     using System.Collections.Generic;
 
     /// <summary>
+    /// The initial development deck the development card appeared in.
+    /// </summary>
+    public enum DevelopmentLevel : byte
+    {
+        /// <summary>
+        /// The first development deck.
+        /// </summary>
+        Level1 = 0,
+
+        /// <summary>
+        /// The second development deck.
+        /// </summary>
+        Level2 = 1,
+
+        /// <summary>
+        /// The third development deck.
+        /// </summary>
+        Level3 = 2,
+    }
+
+    /// <summary>
     /// Describes a development card.
     /// </summary>
     public sealed class DevelopmentCard : IComparable<DevelopmentCard>, ITokenFormattable
@@ -13,11 +34,13 @@ namespace GameTheory.Games.Splendor
         /// <summary>
         /// Initializes a new instance of the <see cref="DevelopmentCard"/> class.
         /// </summary>
+        /// <param name="level">The initial development deck the development card appeared in.</param>
         /// <param name="prestige">The prestige awarded by this card.</param>
         /// <param name="bonus">The bonus granted by this card.</param>
         /// <param name="cost">The cost of this card.</param>
-        public DevelopmentCard(int prestige, Token bonus, EnumCollection<Token> cost)
+        public DevelopmentCard(DevelopmentLevel level, int prestige, Token bonus, EnumCollection<Token> cost)
         {
+            this.Level = level;
             this.Prestige = prestige;
             this.Cost = cost;
             this.Bonus = bonus;
@@ -26,6 +49,7 @@ namespace GameTheory.Games.Splendor
         /// <summary>
         /// Initializes a new instance of the <see cref="DevelopmentCard"/> class.
         /// </summary>
+        /// <param name="level">The initial development deck the development card appeared in.</param>
         /// <param name="prestige">The prestige awarded by this card.</param>
         /// <param name="bonus">The bonus granted by this card.</param>
         /// <param name="diamond">The diamond component of the cost.</param>
@@ -33,8 +57,8 @@ namespace GameTheory.Games.Splendor
         /// <param name="emerald">The emerald component of the cost.</param>
         /// <param name="ruby">The ruby component of the cost.</param>
         /// <param name="onyx">The onyx component of the cost.</param>
-        public DevelopmentCard(int prestige, Token bonus, int diamond = 0, int sapphire = 0, int emerald = 0, int ruby = 0, int onyx = 0)
-            : this(prestige, bonus, EnumCollection<Token>.Empty
+        public DevelopmentCard(DevelopmentLevel level, int prestige, Token bonus, int diamond = 0, int sapphire = 0, int emerald = 0, int ruby = 0, int onyx = 0)
+            : this(level, prestige, bonus, EnumCollection<Token>.Empty
                   .Add(Token.Diamond, diamond)
                   .Add(Token.Sapphire, sapphire)
                   .Add(Token.Emerald, emerald)
@@ -57,6 +81,11 @@ namespace GameTheory.Games.Splendor
         public IList<object> FormatTokens => this.Prestige > 0
             ? new object[] { this.Bonus, " +", this.Prestige }
             : new object[] { this.Bonus };
+
+        /// <summary>
+        /// Gets the initial development deck the development card appeared in.
+        /// </summary>
+        public DevelopmentLevel Level { get; }
 
         /// <summary>
         /// Gets the prestige awarded by this card.
