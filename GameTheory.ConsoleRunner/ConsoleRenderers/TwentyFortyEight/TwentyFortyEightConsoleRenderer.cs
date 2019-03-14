1a1092b0962f4941bbb895baa550f4849e5e3de9
John Gietzen <john@gietzen.us>
2019-03-13 20:57:07 -0700

    Show 2048 numbers in color.

--- GameTheory.ConsoleRunner/ConsoleRenderers/TwentyFortyEight/TwentyFortyEightConsoleRenderer.cs
+++ GameTheory.ConsoleRunner/ConsoleRenderers/TwentyFortyEight/TwentyFortyEightConsoleRenderer.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
 
 namespace GameTheory.ConsoleRunner.ConsoleRenderers.TwentyFortyEight
 {
@@ -19,6 +19,45 @@ namespace GameTheory.ConsoleRunner.ConsoleRenderers.TwentyFortyEight
             if (token is MoveDirection move)
             {
                 Console.Write(Resources.ResourceManager.GetEnumString(move));
+            }
+            else if (token is string str && int.TryParse(str, out var value))
+            {
+                ConsoleColor color;
+                switch (value)
+                {
+                    case 2:
+                    case 4:
+                    default:
+                        color = ConsoleColor.Gray;
+                        break;
+
+                    case 8:
+                    case 16:
+                        color = ConsoleColor.DarkYellow;
+                        break;
+
+                    case 32:
+                    case 64:
+                        color = ConsoleColor.Red;
+                        break;
+
+                    case 128:
+                    case 256:
+                    case 512:
+                    case 1024:
+                    case 2048:
+                        color = ConsoleColor.Yellow;
+                        break;
+
+                    case var _ when value > 2048:
+                        color = ConsoleColor.DarkGreen;
+                        break;
+                }
+
+                ConsoleInteraction.WithColor(color, () =>
+                {
+                    base.RenderToken(state, token);
+                });
             }
             else
             {
