2374b2258d906a249853dd0cb7659bf5e2152264
John Gietzen <john@gietzen.us>
2019-09-08 09:39:17 -0700

    Added a windows forms project.

---
+++ GameTheory.FormsRunner/GameManagerForm.cs
@@ -0,0 +1,20 @@
+// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GameTheory.FormsRunner
+{
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Tracks ongoing games and players.
+    /// </summary>
+    public partial class GameManagerForm : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameManagerForm"/> class.
+        /// </summary>
+        public GameManagerForm()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ GameTheory.FormsRunner/Program.cs
@@ -0,0 +1,24 @@
+// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GameTheory.FormsRunner
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Contains the main entry point for the application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GameManagerForm());
+        }
+    }
+}
