aff0bf312c85359e792cb0e13b3e3406b593b67e
Katie Gietzen <katie@gietzen.us>
2019-10-05 18:50:41 -0700

    Updated the GameTemplate to include basic GameState and Move classes.

---
+++ GameTheory.GameTemplate/GameTheory.Games.$game$/Move.cs
@@ -0,0 +1,34 @@
+﻿// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+
+namespace GameTheory.Games.$game$
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents a move in $game$.
+    /// </summary>
+    public sealed class Move : IMove
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Move"/> class.
+        /// </summary>
+        /// <param name="playerToken">The player who may make this move.</param>
+        public Move(PlayerToken playerToken)
+        {
+            this.PlayerToken = playerToken;
+        }
+
+        /// <inheritdoc />
+        public IList<object> FormatTokens => throw new NotImplementedException();
+
+        /// <inheritdoc />
+        public bool IsDeterministic => true;
+
+        /// <inheritdoc />
+        public PlayerToken PlayerToken { get; }
+
+        /// <inheritdoc />
+        public override string ToString() => string.Concat(this.FlattenFormatTokens());
+    }
+}
