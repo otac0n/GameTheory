ac1e9e75045ba57c2eaca30ce36371fad2bcfa1c
John Gietzen <john@gietzen.us>
2019-10-11 23:44:45 -0700

    Moved Tic-tac-toe tests into a separate assembly.

--- GameTheory.Tests/Strategies/ImmediateWinStrategyTests.cs
+++ GameTheory.Tests/Strategies/ImmediateWinStrategyTests.cs
@@ -1,7 +1,8 @@
-﻿// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
+// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
 
 namespace GameTheory.Tests.Strategies
 {
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using GameTheory.Games.TicTacToe;
@@ -16,7 +17,7 @@ namespace GameTheory.Tests.Strategies
         [TestCase("0,0; 1,1; 2,2; 1,0; 2,0", "1,2")]
         public async Task GetMove_WhenThereIsAWinningMove_ReturnsAWinningMove(string moveList, string expectedMove)
         {
-            var state = Games.TicTacToeTests.ApplyMoves(new GameState(), moveList);
+            var state = ApplyMoves(new GameState(), moveList);
             using (var strategy = new ImmediateWinStrategy<Move>())
             {
                 var maybeMove = await strategy.ChooseMove(state, state.ActivePlayer, CancellationToken.None);
@@ -35,12 +36,24 @@ namespace GameTheory.Tests.Strategies
         [TestCase("0,0; 1,1; 2,2; 2,1; 0,1; 0,2; 2,0; 1,0")]
         public async Task GetMove_WhenThereIsNoWinningMove_ReturnsAnEmptyValue(string moveList)
         {
-            var state = Games.TicTacToeTests.ApplyMoves(new GameState(), moveList);
+            var state = ApplyMoves(new GameState(), moveList);
             using (var strategy = new ImmediateWinStrategy<Move>())
             {
                 var maybeMove = await strategy.ChooseMove(state, state.ActivePlayer, CancellationToken.None);
                 Assert.That(maybeMove.HasValue, Is.False);
             }
+        }
+
+        internal static T ApplyMoves<T>(T state, string moveList)
+            where T : IGameState<Move>
+        {
+            var moves = from move in moveList.Split(';')
+                        let parts = move.Split(',')
+                        let x = int.Parse(parts[0].Trim())
+                        let y = int.Parse(parts[1].Trim())
+                        select new { x, y };
+
+            return (T)moves.Aggregate((IGameState<Move>)state, (s, m) => s.MakeMove(s.GetAvailableMoves().Single(a => a.X == m.x && a.Y == m.y)));
         }
     }
 }
