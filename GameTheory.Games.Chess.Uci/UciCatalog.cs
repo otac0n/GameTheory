f0250c707b0911ebdff731c8b527f02d6f5e9a25
John Gietzen <john@gietzen.us>
2019-11-16 20:05:53 -0800

    Create initializer parameters based on the engine options.

--- GameTheory.Games.Chess.Uci/UciCatalog.cs
+++ GameTheory.Games.Chess.Uci/UciCatalog.cs
@@ -7,6 +7,7 @@ namespace GameTheory.Games.Chess.Uci
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.ComponentModel.DataAnnotations;
     using System.IO;
     using System.Linq;
     using System.Reflection;
@@ -43,10 +44,7 @@ namespace GameTheory.Games.Chess.Uci
                 {
                     return new ReadOnlyCollection<Initializer>(new[]
                     {
-                        new Initializer(SharedResources.DefaultInstance, parameters => new UciPlayer((PlayerToken)parameters[0], this.path, null, null), new[]
-                        {
-                            new DynamicParameterInfo("playerToken", typeof(PlayerToken), 0, ParameterAttributes.None, false, null, null),
-                        }),
+                        CreateInitializer(this.path, this.engine.Options),
                     });
                 });
             }
@@ -64,6 +62,67 @@ namespace GameTheory.Games.Chess.Uci
             public void Dispose()
             {
                 this.engine.Dispose();
+            }
+
+            private static Initializer CreateInitializer(string path, IEnumerable<OptionCommand> options)
+            {
+                var member = typeof(UciPlayer);
+                var position = 0;
+                var parameters = new List<ParameterInfo>
+                {
+                    new DynamicParameterInfo("playerToken", typeof(PlayerToken), position++, false, null, member, null),
+                };
+
+                foreach (var option in options)
+                {
+                    object defaultValue = null;
+                    var hasDefault = false;
+                    CustomAttributeData[] attributes = null;
+
+                    switch (option.Type)
+                    {
+                        case "check":
+                            if (hasDefault = bool.TryParse(option.Default, out var defaultBool))
+                            {
+                                defaultValue = defaultBool;
+                            }
+
+                            parameters.Add(new DynamicParameterInfo(option.Name, typeof(bool), position++, hasDefault, defaultValue, member, attributes));
+                            break;
+
+                        case "spin":
+                            if (hasDefault = int.TryParse(option.Default, out var defaultInt))
+                            {
+                                defaultValue = defaultInt;
+                            }
+
+                            if (int.TryParse(option.Min, out var minInt) && int.TryParse(option.Max, out var maxInt))
+                            {
+                                attributes = new[] { new DynamicAttributeData(typeof(RangeAttribute).GetConstructor(new[] { typeof(int), typeof(int) }), new object[] { minInt, maxInt }) };
+                            }
+
+                            parameters.Add(new DynamicParameterInfo(option.Name, typeof(int), position++, hasDefault, defaultValue, member, attributes));
+                            break;
+
+                        case "combo":
+                            parameters.Add(new DynamicParameterInfo(option.Name, typeof(string), position++, option.Default is string, option.Default, member, attributes));
+                            break;
+
+                        case "string":
+                            if (option.Min != null && option.Max != null)
+                            {
+                                attributes = new[] { new DynamicAttributeData(typeof(RangeAttribute).GetConstructor(new[] { typeof(Type), typeof(string), typeof(string) }), new object[] { typeof(string), option.Min, option.Max }) };
+                            }
+
+                            parameters.Add(new DynamicParameterInfo(option.Name, typeof(string), position++, option.Default is string, option.Default, member, attributes));
+                            break;
+
+                        default:
+                            continue;
+                    }
+                }
+
+                return new Initializer(FormatUtilities.FormatList(parameters.Select(p => p.Name)), args => new UciPlayer((PlayerToken)args[0], path, null, null), parameters);
             }
         }
     }
