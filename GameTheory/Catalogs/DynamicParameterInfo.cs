47cd359722bb3f2688c1d3f20667aa7477eaf912
John Gietzen <john@gietzen.us>
2019-11-16 19:40:11 -0800

    Added custom attribute support.

--- GameTheory/Catalogs/DynamicParameterInfo.cs
+++ GameTheory/Catalogs/DynamicParameterInfo.cs
@@ -3,26 +3,45 @@
 namespace GameTheory.Catalogs
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
 
+    /// <summary>
+    /// A convenience class implementing <see cref="ParameterInfo"/>.
+    /// </summary>
     public class DynamicParameterInfo : ParameterInfo
     {
+        private readonly IList<CustomAttributeData> customAttributes;
         private readonly bool hasDefaultValue;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DynamicParameterInfo"/> class.
+        /// </summary>
+        /// <param name="name">The name of this parameter.</param>
+        /// <param name="parameterType">The Type of the parameter.</param>
+        /// <param name="position">The zero-based position of the parameter in the parameter list.</param>
+        /// <param name="hasDefaultValue">A value that indicates whether this parameter has a default value.</param>
+        /// <param name="defaultValue">A value indicating the default value if the parameter has a default value.</param>
+        /// <param name="member">A value indicating the member in which the parameter is implemented.</param>
+        /// <param name="customAttribtues">A collection that contains this parameter's custom attributes.</param>
         public DynamicParameterInfo(
             string name,
-            Type @class,
+            Type parameterType,
             int position,
-            ParameterAttributes attributes,
             bool hasDefaultValue,
             object defaultValue,
-            MemberInfo member)
+            MemberInfo member,
+            CustomAttributeData[] customAttribtues)
         {
             this.NameImpl = name;
-            this.ClassImpl = @class;
+            this.ClassImpl = parameterType;
             this.PositionImpl = position;
-            this.AttrsImpl = attributes;
+            this.AttrsImpl = hasDefaultValue ? ParameterAttributes.HasDefault : ParameterAttributes.None;
             this.MemberImpl = member;
+            this.customAttributes = customAttribtues == null
+                ? Array.Empty<CustomAttributeData>()
+                : (IList<CustomAttributeData>)customAttribtues.ToList().AsReadOnly();
 
             this.hasDefaultValue = hasDefaultValue;
             if (hasDefaultValue)
@@ -35,6 +54,34 @@ namespace GameTheory.Catalogs
             }
         }
 
+        /// <inheritdoc/>
+        public override object DefaultValue => this.hasDefaultValue ? this.DefaultValueImpl : throw new InvalidOperationException();
+
+        /// <inheritdoc/>
         public override bool HasDefaultValue => this.hasDefaultValue;
+
+        /// <inheritdoc/>
+        public override object RawDefaultValue => this.DefaultValue;
+
+        /// <inheritdoc/>
+        public override object[] GetCustomAttributes(bool inherit) =>
+            this.GetCustomAttributesData().Select(Create).ToArray();
+
+        /// <inheritdoc/>
+        public override object[] GetCustomAttributes(Type attributeType, bool inherit) =>
+            this.GetCustomAttributesData().Where(Filter(attributeType)).Select(Create).ToArray();
+
+        /// <inheritdoc/>
+        public override IList<CustomAttributeData> GetCustomAttributesData() => this.customAttributes;
+
+        /// <inheritdoc/>
+        public override bool IsDefined(Type attributeType, bool inherit) =>
+            this.GetCustomAttributesData().Any(Filter(attributeType));
+
+        private static Attribute Create(CustomAttributeData attribute) =>
+            (Attribute)attribute.Constructor.Invoke(attribute.ConstructorArguments.Select(c => c.Value).ToArray());
+
+        private static Func<CustomAttributeData, bool> Filter(Type attributeType) =>
+            a => attributeType.IsAssignableFrom(a.AttributeType);
     }
 }
