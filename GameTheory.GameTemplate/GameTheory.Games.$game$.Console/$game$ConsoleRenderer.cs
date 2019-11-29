269fe1332882ab50798d2d64007197201f4e36f6
John Gietzen <john@gietzen.us>
2019-11-29 15:24:37 -0800

    Fix project template.
    (cherry picked from commit 81f25a11879380998dadad94e5305131cc62d1f2)

--- GameTheory.GameTemplate/GameTheory.Games.$game$.Console/$game$ConsoleRenderer.cs
+++ GameTheory.GameTemplate/GameTheory.Games.$game$.Console/$game$ConsoleRenderer.cs
@@ -8,13 +8,13 @@ namespace GameTheory.Games.$game$.Console
     /// <summary>
     /// Provides a console renderer for the game of <see cref="GameState">$game$</see>.
     /// </summary>
-    public class $game$ConsoleRenderer : BaseConsoleRenderer<Move>
+    public class $game$ConsoleRenderer : ConsoleRendererBase<GameState, Move>
     {
         /// <inheritdoc/>
-        public override void Show(IGameState<Move> state, PlayerToken playerToken = null) => new Templates(playerToken).RenderGameState((GameState)state, this.MakeRenderTokenWriter(state));
+        public override void Show(GameState state, PlayerToken playerToken = null) => new Templates(playerToken).RenderGameState((GameState)state, this.MakeRenderTokenWriter(state));
 
         /// <inheritdoc/>
-        protected override void RenderToken(IGameState<Move> state, object token)
+        protected override void RenderToken(GameState state, object token)
         {
             if (state == null)
             {
