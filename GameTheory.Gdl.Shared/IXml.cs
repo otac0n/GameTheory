d688d04572faff1b640b5936875f452894e2086c
John Gietzen <john@gietzen.us>
2019-12-01 21:04:57 -0800

    Fix style issues.

--- GameTheory.Gdl.Shared/IXml.cs
+++ GameTheory.Gdl.Shared/IXml.cs
@@ -1,18 +1,20 @@
-using System.Threading.Tasks;
-using System.Xml;
+// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
 
 namespace GameTheory.Gdl.Shared
 {
+    using System.Threading.Tasks;
+    using System.Xml;
+
     /// <summary>
     /// An object that can be written to an xml writer.
     /// </summary>
     public interface IXml
     {
         /// <summary>
-        /// Writes the object to the
+        /// Writes the object to the specified writer.
         /// </summary>
-        /// <param name="writer"></param>
-        /// <returns></returns>
+        /// <param name="writer">The <see cref="XmlWriter"/> to write to.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         Task ToXml(XmlWriter writer);
     }
 }
