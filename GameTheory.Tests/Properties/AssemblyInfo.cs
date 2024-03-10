c7a9a1ca9775225f9ccd9c57691cf2dcb0896c6a
John Gietzen <john@gietzen.us>
2024-03-10 13:23:01 -0700

    Updated projects to dotnet 8 and the latest project template.

--- GameTheory.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright © John & Katie Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
-
-using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GameTheory")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("5f27b637-0b38-4806-92aa-3faeccfcc225")]
